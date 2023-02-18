bd92757e85d046e3f3a80979897ae487bc2cf6fb
Dan <107619540+MrEzerYT@users.noreply.github.com>
2023-02-18 21:00:53 +0300

    Create animation-text.js

---
+++ assets/js/animation-text.js
@@ -0,0 +1,15 @@
+var text = "Моё портфолио";
+var delay = 100; // cкорость
+var elem = document.getElementById("main-title");
+ 
+var print_text = function(text, elem, delay) {
+    if(text.length > 0) {
+        elem.innerHTML += text[0];
+        setTimeout(
+            function() {
+                print_text(text.slice(1), elem, delay); 
+            }, delay
+        );
+    }
+}
+print_text(text, elem, delay);
